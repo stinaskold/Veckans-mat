0fca5941144b7b4b1aebd3d5a58f3d5f74193dc5
stinaskold <Stina@Stinas-iMac.lan>
2016-06-02 11:25:03 +0200

    Installerade Mocha och Chai

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,37 @@ var Dish = require('../models/db');
 var router = express.Router();
 var newDish;
 
-router.get('/', function(req, res) {
-  Dish.find((function(err, result) {
+// router.get('/', function(req, res) {
+//   Dish.find((function(err, result) {
+//   if (err) return console.log(err)
+//   console.log(result);
+//   res.render('index.pug', {dishes: result})
+//   }))
+// })
+
+
+
+// Dish.find(function(err, result) {
+// if (err) return console.log(err)
+// console.log(result);
+// //res.render('index.pug', {dishes: result})
+// });
+
+
+  Dish.aggregate([
+    {$project: { _id: 0, keywords: 1 }},
+    {$unwind: "$keywords" },
+    {$group: {"_id": "$keywords", "keywords":{"$addToSet": "$keywords"}}},
+    {$unwind: "$keywords" },
+    {$group: {"_id": "$keywords", count: {$sum: 1}}},
+    {$project: { _id: 0, keywords: "$_id" }}
+  ], function(err, result) {
   if (err) return console.log(err)
-  console.log(result);
-  res.render('index.pug', {dishes: result})
-  }))
-})
+  console.log('%j',result);
+
+  //res.render('index.pug', {dishes: result})
+  });
+
 
 
 router.post('/dishes', function(req, res) {
